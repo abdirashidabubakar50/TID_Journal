8f2f39ff6ed94c8f6f5d6b7a087f0e061403f219
ABDIRASHID <abdirashidabubakar7@gmail.com>
2025-07-14 01:11:35 +0300

    initial commit | create user model

---
+++ app/models/User.py
@@ -0,0 +1,27 @@
+from pydantic import BaseModel, EmailStr, Field, model_validator
+from ..schemas.schemas import PyObjectId
+
+class UserRegister(BaseModel):
+    username: str = Field(..., min_length=3)
+    email: EmailStr
+    password: str = Field(..., min_length=6)
+    confirm_password: str = Field(..., min_length=6)
+
+    @model_validator(mode='after')
+    def check_password_match(self) -> 'UserRegister':
+        pw1 = self.password
+        pw2 = self.confirm_password
+        if pw1 is not None and pw2 is not None and pw1 != pw2:
+            raise ValueError("Passwords do not match")
+        return self
+class User(BaseModel):
+    username: str
+    email: str
+
+class UserInDB(User):
+    id: PyObjectId = Field(default_factory=PyObjectId, alias="_id")
+    hashed_password: str
+
+class LoginRequest(BaseModel):
+    email: str
+    password: str
