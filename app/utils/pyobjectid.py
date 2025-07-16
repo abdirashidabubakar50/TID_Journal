9562aca719f3e384c328b2fc7c6f20952c2a2d87
ABDIRASHID <abdirashidabubakar7@gmail.com>
2025-07-17 00:16:50 +0300

    PyobjectId class to serialize ObjectId for easy use with pydantic models

---
+++ app/utils/pyobjectid.py
@@ -0,0 +1,30 @@
+from pydantic import BaseModel, Field
+from bson import ObjectId
+from pydantic import GetJsonSchemaHandler
+from pydantic_core import core_schema
+from typing import Any
+
+
+
+class PyObjectId(ObjectId):
+    @classmethod
+    def __get_pydantic_core_schema__(
+        cls, source_type: Any, handler: Any
+    ) -> core_schema.CoreSchema:
+        return core_schema.no_info_after_validator_function(
+            cls.validate,
+            core_schema.str_schema(),
+            serialization=core_schema.plain_serializer_function_ser_schema(str),
+        )
+
+    @classmethod
+    def validate(cls, v: Any) -> ObjectId:
+        if not ObjectId.is_valid(v):
+            raise ValueError("Invalid ObjectId")
+        return ObjectId(v)
+
+    @classmethod
+    def __get_pydantic_json_schema__(
+        cls, core_schema: core_schema.CoreSchema, handler: GetJsonSchemaHandler
+    ) -> dict:
+        return {"type": "string"}
